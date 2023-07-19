524a7d47c7a74e9d0b085f6d1d3487aa390ccb8c
k8 <katesterchi@gmail.com>
2023-07-19 12:22:45 -0400

    added function to load user input from local storage

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -6,10 +6,10 @@ $(function () {
   function updateTimeBlocks() {
     const currentHour = dayjs().hour(); // Get the current hour (0-23) using dayjs
 
-    // Log the current hour to the console
+  // Log the current hour to the console
     console.log("Current Hour:", currentHour);
 
-    // Loop through each time block
+  // Loop through each time block
     $(".time-block").each(function () {
       const hour = parseInt($(this).attr("id").split("-")[1]); // Extract the hour from the id
       if (hour < currentHour) {
@@ -22,39 +22,39 @@ $(function () {
     });
   }
 
+   // Function to load user input from localStorage
+   function loadUserInput() {
+    $(".time-block").each(function () {
+      const hourId = $(this).attr("id"); // Get the "hour-x" id of the time-block
+      const savedDescription = localStorage.getItem(hourId); // Retrieve the saved description from localStorage
+
+      // Set the value of the corresponding textarea element
+      $(this).find(".description").val(savedDescription);
+    });
+  }
+
   // Call the updateTimeBlocks function on page load
   $(document).ready(function () {
     updateTimeBlocks();
 
-    // Call the updateTimeBlocks function every minute to keep the styles up-to-date
+  // Call the updateTimeBlocks function every minute to keep the styles up-to-date
     setInterval(updateTimeBlocks, 60000); // 60000 milliseconds = 1 minute
+ // Load user input from localStorage on page load
+ loadUserInput();
   });
-  // Click listener for the save buttons
+  // Click listener for the save buttons--
+  // In the click listener function, $(this) refers to the clicked save button. 
+  // Using the siblings() method, traverse DOM to find the sibling element with 
+  // the class description, which is the textarea containing the user input. 
+  // then, use the val() method to get the value of the textarea, which is the user input.
   $(".saveBtn").on("click", function () {
-    const description = $(this).siblings(".description").val(); // Get the value from the textarea
-    const hourId = $(this).parent().attr("id"); // Get the "hour-x" id of the time-block
-    localStorage.setItem(hourId, description); // Use the "hour-x" id as the key to save the description in local storage
+    const hourId = $(this).closest(".time-block").attr("id"); // Get the "hour-x" id of the containing time-block
+    const userInput = $(this).siblings(".description").val(); // Get the user input from the textarea
 
-    // Log the saved data to the console
-    console.log(`Saved: ${description}`);
+    // Save the user input to localStorage using the hourId as the key
+    localStorage.setItem(hourId, userInput);
+
+    console.log("User input saved:", userInput);
   });
 });
 
-// TODO: Add a listener for click events on the save button. This code should
-// use the id in the containing time-block as a key to save the user input in
-// local storage. HINT: What does `this` reference in the click listener
-// function? How can DOM traversal be used to get the "hour-x" id of the
-// time-block containing the button that was clicked? How might the id be
-// useful when saving the description in local storage?
-//
-// TODO: Add code to apply the past, present, or future class to each time
-// block by comparing the id to the current hour. HINTS: How can the id
-// attribute of each time-block be used to conditionally add or remove the
-// past, present, and future classes? How can Day.js be used to get the
-// current hour in 24-hour time?
-//
-// TODO: Add code to get any user input that was saved in localStorage and set
-// the values of the corresponding textarea elements. HINT: How can the id
-// attribute of each time-block be used to do this?
-//
-// TODO: Add code to display the current date in the header of the page.
